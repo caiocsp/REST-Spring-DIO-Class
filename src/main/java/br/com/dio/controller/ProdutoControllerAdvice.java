ee9d68d03b0c5cce23d244cf5905fad93ce25a53
caiocsp <caio.c.paulino@gmail.com>
2022-10-31 00:11:47 -0300

    Add Exception Handlers - Aula1_etapa 4

---
+++ src/main/java/br/com/dio/controller/ProdutoControllerAdvice.java
@@ -0,0 +1,42 @@
+package br.com.dio.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.dio.exception.ProductNullException;
+import br.com.dio.exception.ProductPriceException;
+
+@ControllerAdvice
+public class ProdutoControllerAdvice extends ResponseEntityExceptionHandler {
+//Teremos um controller de erros    
+//Garantindo que não salve se houver erros na requisição
+//Seja informando de forma mais genérica ou mais específica como os exemplos abaixo
+
+    @ExceptionHandler(ProductNullException.class) //Definindo responsável pela exceção
+    public ResponseEntity<Object> capturaErroNull(){
+        
+        Map<String, Object> body = new HashMap<String, Object>();
+        
+        body.put("message","Verifique os campos do produto");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+        
+    }
+
+    @ExceptionHandler(ProductPriceException.class) //Definindo responsável pela exceção
+    public ResponseEntity<Object> capturaErroPreco(){
+        
+        Map<String, Object> body = new HashMap<String, Object>();
+        
+        body.put("message","Verifique o preço do produto");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+        
+    }
+}
